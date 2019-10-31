a78ca0365b8c2d58c9ddea16eaf6fd57393995bd
vitius1 <36272679+vitius1@users.noreply.github.com>
2019-10-31 12:58:54 +0100

    adding connection to civicrm database

--- contactlistmodule/src/Form/Contactlist.php
+++ contactlistmodule/src/Form/Contactlist.php
@@ -9,59 +9,77 @@ use Drupal\Core\Database\Database;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use \Symfony\Component\HttpFoundation\Response;
-/**
- * Provides contact list form.
- */
+use Drupal\cmrf_core\Core;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 class Contactlist extends FormBase {
-public function getFormId() {
-    return 'contact_list';
-  }
-  protected function load($header) {
-    $select = Database::getConnection()->select('civicrm_contact', 'c');
-    $select->join('civicrm_email', 'e', 'c.id = e.contact_id');
-    $select->join('civicrm_phone', 'p', 'c.id = p.contact_id');
-    $select->addField('c', 'display_name' );
-    $select->addField('e', 'email');
-    $select->addField('p', 'phone');
-    $select->addField('c', 'id');
-    $select = $select->extend('Drupal\Core\Database\Query\TableSortExtender');
-    $select = $select->orderByHeader($header);
-    $select = $select->extend('Drupal\Core\Database\Query\PagerSelectExtender')->limit(20);
-    $entries = $select->execute()->fetchAll(\PDO::FETCH_ASSOC);
-    return $entries;
+  public function getFormId() {
+      return 'contact_list';
+    }
+
+  /**
+  * get cmrf core
+  */
+  public static function create(ContainerInterface $container) {
+   $core = $container->get('cmrf_core.core');
+   return new static($core);
   }
 
+  public function __construct(core $core) {
+       $this->core = $core;
+  }
+
+  private function connector() {
+   return \Drupal::config('cmrf_example.settings')->get('connector');
+  }
+
+  /**
+  * connect to civicrm api3
+  */
+  public function getContactIds() {
+   $call=$this->core->createCall($this->connector(),'Contact','get', ['return' => ['display_name, email, phone, id'], 'sort' =>  'display_name DESC ' , 'offset' => 10, 'limit' => 50],array());
+   $this->core->executeCall($call);
+   return $call->getReply();
+}
+
+  /**
+  * form
+  */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $header = array(
       'name' => array('data' => t('Name'), 'field' => 'c.display_name'),
       'email' => array('data' => t('Email'), 'field' => 'e.email'),
-      'phone' => array('data' => t('Phone'), 'field' => 'p.phone',  'sort' => 'DESC'),
+      'phone' => array('data' => t('Phone'), 'field' => 'p.phone'),
     );
 
-    foreach ($entries = $this->load($header) as $entry) {
-      $name = array('data' => $entry['display_name'], 'class' => 'link', 'id' => $entry['id']);
-      $email = array('data' => $entry['email'], 'class' => 'link', 'id' => $entry['id']);
-      $phone = array('data' => $entry['phone'], 'class' => 'link', 'id' => $entry['id']);
-      $profile = array('data' => t(""), 'colspan' => '3');
-      $loading = array('data' => t("LOADING"), 'colspan' => '3');
-      $attributes = array('class' => array('content'));
-      $profile_id = 'profile_' . $entry['id'];
-      $loading_id = 'loading_' . $entry['id'];
+    foreach ($entries = $this->getContactIds() as $entry2) {
+      if (is_array($entry2) || $entry2 instanceof Traversable) {
+        foreach ($entry2 as $entry) {
+          $name = array('data' => $entry['display_name'], 'class' => 'link', 'id' => $entry['id']);
+          $email = array('data' => $entry['email'], 'class' => 'link', 'id' => $entry['id']);
+          $phone = array('data' => $entry['phone'], 'class' => 'link', 'id' => $entry['id']);
+          $profile = array('data' => t(""), 'colspan' => '3');
+          $loading = array('data' => t("LOADING"), 'colspan' => '3');
+          $attributes = array('class' => array('content'));
+          $profile_id = 'profile_' . $entry['id'];
+          $loading_id = 'loading_' . $entry['id'];
 
-      $rows[$entry['id']] = array(
-        'name' => $name,
-        'email' => $email,
-        'phone' => $phone,
-        '#attributes' => $attributes
-      );   // row with basic info
-      $rows[$profile_id] = array(
-        'name' => $profile,
-        '#attributes' => array('class' => array('profile', $profile_id))
-      );   // profile row, hidden in default
-      $rows[$loading_id] = array(
-        'name' => $loading,
-        '#attributes' => array('class' => array('loading', $loading_id))
-      );   // loading row hidden in default
+          $rows[$entry['id']] = array(
+            'name' => $name,
+            'email' => $email,
+            'phone' => $phone,
+            '#attributes' => $attributes
+          );   // row with basic info
+          $rows[$profile_id] = array(
+            'name' => $profile,
+            '#attributes' => array('class' => array('profile', $profile_id))
+          );   // profile row, hidden in default
+          $rows[$loading_id] = array(
+            'name' => $loading,
+            '#attributes' => array('class' => array('loading', $loading_id))
+          );   // loading row hidden in default
+        }
+      }
     }
 
     $form['table'] = array(
@@ -73,9 +91,6 @@ public function getFormId() {
     );
     // do not cache this page.
     $form['$cache']['max-age'] = 0;
-    $form['pager'] = array(
-      '#type' => 'pager'
-    );
     $form['submit'] = array(
       '#type' => 'submit',
       '#value' => t('submit'),
@@ -83,17 +98,14 @@ public function getFormId() {
 
     return $form;
   }
+  
   public function validateForm(array &$form, FormStateInterface $form_state) {
-
   }
+
   public function submitForm(array &$form, FormStateInterface $form_state) {
     drupal_set_message(t('The form is working.'));
   }
 
   public function processTable(&$element, FormStateInterface $form_state, &$complete_form) {
-
-
   }
-
-
 }
